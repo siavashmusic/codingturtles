e12b212926a9044f5fa4b490aa794360d7117a41
Siavash Aghili <siavashaghili@gmail.com>
2023-02-09 16:05:17 +0000

    datesource and functions update

--- application/routes.py
+++ application/routes.py
@@ -3,17 +3,25 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-
-@app.route("/css")
-def css():
-    return render_template("css.html")
+    return render_template("index.html", login= True)
 
 
 @app.route("/html")
 def html():
-    return render_template("html.html")
+    return render_template("articles.html")
+
+
+@app.route("/css")
+def css():
+    courseData = [
+        {"courseID":"css", "title":"CSS Syntax", "desc": "A CSS rule consists of a selector and a declaration block.", "img":"static/img/cssSyntax.png", "postBy": "admin", "pubDate":" JUL 5TH '22" },
+        {"courseID":"css", "title":"CSS Selectors", "desc": "A CSS selector selects the HTML element(s) you want to style.", "img":"static/img/CSSSelectors.png", "postBy": "admin", "pubDate":" JUL 5TH '22" },
+        {"courseID":"css", "title":"CSS Backgrounds", "desc": "The CSS background properties are used to add background effects for elements.", "img":"static/img/CSSBackgrounds.png", "postBy": "admin", "pubDate":" JUL 5TH '22" },
+        {"courseID":"css", "title":"CSS Borders", "desc": "The CSS border properties allow you to specify the style, width, and color of an element's border.", "img":"static/img/CSSBorders.png", "postBy": "admin", "pubDate":" JUL 5TH '22" },
+    
+    ]
+    print(courseData)
+    return render_template("articles.html", courseData=courseData)
 
 
 @app.route("/sql")
@@ -32,10 +40,10 @@ def javascript():
 
 
 @app.route("/signup")
-@app.route("/register")
-@app.route("/registration")
+# @app.route("/register")
+# @app.route("/registration")
 def signup():
-    return render_template("signup.html", login=False)
+    return render_template("signup.html", login=True)
 
 
 @app.route("/login")
