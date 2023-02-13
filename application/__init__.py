98e7ca94d972c1689f46d1381c7c9fc23620d2b6
Siavash Aghili <siavashaghili@gmail.com>
2023-02-13 21:11:30 +0000

    postgresql added

--- application/__init__.py
+++ application/__init__.py
@@ -18,7 +18,9 @@ db = SQLAlchemy()
 def create_app():
 
     app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
-    app.config['SQLALCHEMY_DATABASE_URI'] = F"sqlite:///{DB_NAME}"
+    #app.config['SQLALCHEMY_DATABASE_URI'] = F"sqlite:///{DB_NAME}"
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://u4bc1v5jfnkk79:p81fb17c0b8d125dc51939e2d40947cbaa365ce2341d3c18101558e4651cb8861@ec2-54-89-81-44.compute-1.amazonaws.com:5432/d6fgdjsmqlbk4f"
     # app.config['MYSQL_HOST'] = 'localhost'
     # app.config['MYSQL_USER'] = 'root'
     # app.config['MYSQL_PASSWORD'] = '1359slSL@'
