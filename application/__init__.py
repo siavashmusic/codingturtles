839b4ab14d5882a944a336f25d73bbec15151841
Siavash Aghili <siavashaghili@gmail.com>
2023-02-13 22:21:29 +0000

    set new env

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,6 @@
-#import MySQLdb
+# import MySQLdb
 from flask import Flask
-#from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
@@ -9,26 +9,26 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-DB_NAME = "database.db"
+# DB_NAME = "database.db"
 db = SQLAlchemy()
 
-#db = MySQL(app)
+
+# db = MySQL(app)
 
 
 def create_app():
-
     app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
-    #app.config['SQLALCHEMY_DATABASE_URI'] = F"sqlite:///{DB_NAME}"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = F"sqlite:///{DB_NAME}"
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://u4bc1v5jfnkk79:p81fb17c0b8d125dc51939e2d40947cbaa365ce2341d3c18101558e4651cb8861@ec2-54-89-81-44.compute-1.amazonaws.com:5432/d6fgdjsmqlbk4f"
+    app.config[
+        'SQLALCHEMY_DATABASE_URI'] = "postgresql://doadmin:AVNS_8FI6ww669_mV2Y4axAX@db-postgresql-lon1-03923-do-user-486561-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
     # app.config['MYSQL_HOST'] = 'localhost'
     # app.config['MYSQL_USER'] = 'root'
     # app.config['MYSQL_PASSWORD'] = '1359slSL@'
     # app.config['MYSQL_DB'] = 'flask'
     db.init_app(app)
 
-
-# REGISTER OUR ROUTS
+    # REGISTER OUR ROUTS
     from .views import views
     from .auth import auth
     app.register_blueprint(views, url_prefix="/")
@@ -50,11 +50,10 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('codingturtles/' + DB_NAME):
-        with app.app_context():
-            db.create_all()
-            print("Created Database!")
-
+    # if not path.exists('codingturtles/' + DB_NAME):
+    #     with app.app_context():
+    app.app_context()
+    db.create_all()
+    print("Created Database!")
 
 # from application import routes
-
