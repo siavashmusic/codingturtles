08a81d85caa8ce5f1ecfc6e2b8601a3b2f552466
Siavash Aghili <siavashaghili@gmail.com>
2023-02-14 12:31:06 +0000

    dotenv added

--- application/__init__.py
+++ application/__init__.py
@@ -1,18 +1,22 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-
+# import the dotenv in order to access .env file
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-DB_NAME = "database.db"
+# DB_NAME = "database.db"
 db = SQLAlchemy()
 
 
 def create_app():
-
+    load_dotenv()
     app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
-    app.config['SQLALCHEMY_DATABASE_URI'] = F"sqlite:///{DB_NAME}"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = F"sqlite:///{DB_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
     db.init_app(app)
 
     # REGISTER OUR ROUTS
@@ -37,7 +41,8 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('codingturtles/' + DB_NAME):
-        with app.app_context():
-            db.create_all()
-            print("Created Database!")
+    # if not path.exists('codingturtles/' + DB_NAME):
+    #     with
+    app.app_context()
+    db.create_all()
+    print("Created Database!")
